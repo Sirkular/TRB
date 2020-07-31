fb36c1ca5b96110438e9c3f0d76d0cb56e791df3
Justin Lui <sirkular.sirkle@hotmail.com>
2020-07-31 01:15:50 -0400

    Update help with info and add commands, also with all commands.

--- help.js
+++ help.js
@@ -1,20 +1,36 @@
 module.exports = function() {
   const help = {};
+  const commands = {
+    "add":  "\`add resource amount character_prefix1 character_prefix2 ...\`\n",
+    "info": "\`info character_prefix1\`\n"
+  };
 
   help.getHelp = function(args) {
-    if (args.length === 0) {
+    let command = args[0];
+    if (!command) {
       return getCommands();
     }
-    else {
+    else if (command === "info") {
       let out = "";
-      // Add output to out.
+      out += commands[command];
+      out += "Get the MXP of character.\n";
+      return out;
+    }
+    else if (command === "add") {
+      let out = "";
+      out += commands[command];
+      out += "Add amount of resource to characters.\n";
+      out += "character_prefix is any prefix of a character\'s name, spaces not allowed. e.g. Irontank has Iron, Iront, Irontank as valid prefixes.";
       return out;
     }
   }
 
   // TODO: Base commands returned on user role.
   function getCommands() {
-    return "The TRB Bot has no commands currently.";
+    let output = "Supported commands:\n";
+    return Object.values(commands).reduce((out, value) => {
+      return out + " - " + value;
+    }, output);
   }
 
   return help;
